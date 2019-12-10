63d1cec59cf83879d0decc316b8551d4e851693a
Mariia Stepanova <54494950+Mariia2505@users.noreply.github.com>
2019-12-10 16:51:53 +0200

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -6,7 +6,6 @@ const logItems = function (array) {
   }
 };
 
-console.log(logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
-console.log(logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]));
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
-// не понимаю почему ф-ия возвращает undefined
